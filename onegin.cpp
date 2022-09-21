71faafc3ed995bbbe8dd602729cd845c0b41d4e1
Troll0ton <111769737+Troll0ton@users.noreply.github.com>
2022-09-21 21:47:41 +0300

    reworked file reader

--- onegin.cpp
+++ onegin.cpp
@@ -6,24 +6,19 @@
 
 //-----------------------------------------------------------------------------
 
-int  compare_strings (const void *first_str, const void *second_str);
+int compare_strings (const void *first_i, const void *second_i);
 
-int  num_of_strings (char *buf, int buf_size);
+int num_of_strings (char *buf, int buf_size);
 
-int  size_of_file (FILE *file);
+int size_of_file (FILE *file);
 
-int  file_reader (char *buf, int buf_size, FILE *file);
+int file_reader (char *buf, int buf_size, FILE *file);
 
-void split_strings (char *buf, int buf_size, struct String* Text);
+int line_detector (char *buf, int buf_size, int *string_num);
 
 //-----------------------------------------------------------------------------
 
-struct String
-{
-    char *str;
-
-    int   str_len;
-};
+char *buf;
 
 //-----------------------------------------------------------------------------
 
@@ -32,24 +27,38 @@ int main ()
     FILE   *file  = fopen ("hamlet.txt", "rb");
     assert (file != NULL);
 
-    int   buf_size = size_of_file (file);
+    int   buf_size = size_of_file (file) + 1;
 
-    char *buf = (char*) calloc (buf_size, sizeof (char));
+    buf = (char*) calloc (buf_size, sizeof (char));
 
     file_reader (buf, buf_size, file);
 
-    struct String Text[11];
+    int *string_num = (int*) calloc (num_of_strings (buf, buf_size), sizeof (int));
 
-    split_strings (buf, buf_size, Text);
+    int num = line_detector (buf, buf_size, string_num);
 
-    printf ("\n%s %d\n", Text[0].str, Text[0].str_len);
+    qsort (string_num, num, sizeof (int), &compare_strings);
+
+    for(int i = 0; i < num; i++)
+    {
+        printf ("\n%s", (buf + *(string_num + i)));
+    }
+
+    printf ("\n");
+
 }
 
 //-----------------------------------------------------------------------------
 
-int compare_strings (const void *first_str, const void *second_str)
+int compare_strings (const void *first_i, const void *second_i)
 {
-    return strcmp ((const char*) first_str, (const char*) second_str);
+    int i1 = *(const int*) first_i;
+    int i2 = *(const int*) second_i;
+
+    char *first_str  = buf + i1;
+    char *second_str = buf + i2;
+
+    return strcmp (first_str, second_str);
 }
 
 //-----------------------------------------------------------------------------
@@ -71,49 +80,6 @@ int num_of_strings (char *buf, int buf_size)
 
 //-----------------------------------------------------------------------------
 
-void split_strings (char *buf, int buf_size, struct String* Text)
-{
-    int max_buf_size = 100;
-    int str_size = 0;
-    int Text_index = 0;
-
-    char *current_string = (char*) calloc (max_buf_size, sizeof (char));
-
-    for(int i = 0; i < buf_size; i++)
-    {
-        if(*(buf + i) != '\0')
-        {
-            current_string[str_size] = *(buf + i);
-            str_size++;
-
-            if(str_size == max_buf_size)
-            {
-                max_buf_size *= 2;
-                current_string = (char*) realloc (current_string, sizeof (char) * max_buf_size);
-            }
-        }
-
-        else
-        {
-            current_string[str_size] = '\0';
-            str_size++;
-
-            char *pointer_copy = current_string;
-            Text[Text_index].str = pointer_copy;
-            Text[Text_index].str_len = str_size - 1;
-
-            free (current_string);
-            max_buf_size = 100;
-            char *current_string = (char*) calloc (max_buf_size, sizeof (char));
-
-            str_size = 0;
-            Text_index++;
-        }
-    }
-}
-
-//-----------------------------------------------------------------------------
-
 int size_of_file (FILE *file)
 {
     struct stat buffer;
@@ -129,7 +95,8 @@ int size_of_file (FILE *file)
 int file_reader (char *buf, int buf_size, FILE *file)
 {
     fread (buf, sizeof (char), buf_size, file);
-    printf ("%s", buf);
+    *(buf + buf_size) = '\0';
+    printf ("%s\n", buf);
 
     int text_size = num_of_strings (buf, buf_size);
 
@@ -137,3 +104,45 @@ int file_reader (char *buf, int buf_size, FILE *file)
 }
 
 //-----------------------------------------------------------------------------
+
+int line_detector (char *buf, int buf_size, int *string_num)
+{
+    int kol = 0;
+    int posit = 0;
+
+    for(int i = 0; i <= buf_size; i++)
+    {
+        if(*(buf + i) == '\n' || *(buf + i) == '\r')
+        {
+            if(*(buf + i - 1) != '\n' && *(buf + i - 1) != '\r' && *(buf + i - 1) != '\0')
+            {
+                *(string_num + kol) = posit;
+                posit = i + 1;
+                kol++;
+            }
+
+            else
+            {
+                posit++;
+            }
+
+            *(buf + i) = '\0';
+        }
+
+        else
+        {
+            if(i == buf_size)
+            {
+                if(*(buf + i - 1) != '\n' && *(buf + i - 1) != '\r' && *(buf + i - 1) != '\0')
+                {
+                    *(string_num + kol) = posit;
+                }
+            }
+        }
+    }
+
+    return (kol + 1);
+}
+
+//-----------------------------------------------------------------------------
+
